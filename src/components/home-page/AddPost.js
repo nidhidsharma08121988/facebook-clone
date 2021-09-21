ff2a2b4634888c4085eed68c8eb80206039ba7de
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-09-21 14:08:23 +0200

    Add buttons send and cancel in AddPost.js

--- src/components/home-page/AddPost.js
+++ src/components/home-page/AddPost.js
@@ -12,20 +12,33 @@ const AddPost = props => {
   }, [props.currentUser]);
   return (
     <div className={classes.addPostContainer}>
-      <div data-test='user-visibility' className={classes.userVisibilityImage}>
-        <img src={user.userImg} alt='user' className={classes.userImage} />
-        <div className={classes.userVisibility}>
-          <div>{user.userName}</div>
-          <Visibility />
+      <div className={classes.topMenu}>
+        <div
+          data-test='user-visibility'
+          className={classes.userVisibilityImage}
+        >
+          <img src={user.userImg} alt='user' className={classes.userImage} />
+          <div className={classes.userVisibility}>
+            <div>{user.userName}</div>
+            <Visibility />
+          </div>
+        </div>
+        <div className={classes.rightPostMenu}>
+          <i className={`fas fa-times ${classes.cancelPost}`}> </i>
         </div>
       </div>
       <div className={classes.postTextContainer}>
-        <textarea
-          data-testid='input-post'
-          placeholder={`What's on your mind, ${
-            user.userName && user.userName
-          }?`}
-        />
+        <div className={classes.textAreaContainer}>
+          <textarea
+            data-testid='input-post'
+            placeholder={`What's on your mind, ${
+              user.userName && user.userName
+            }?`}
+          />
+        </div>
+        <div className={classes.postIcon}>
+          <i className={`fas fa-location-arrow`}></i>
+        </div>
       </div>
       <div className={classes.addMoreToPostContainer}>
         <AddMoreToPost />
