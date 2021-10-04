7e3cc2a10019a54f52f65b0f542a6f5afb877021
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-10-04 10:25:56 +0200

    Refactor PostMenu.js

--- src/components/posts/PostMenu.js
+++ src/components/posts/PostMenu.js
@@ -1,21 +1,43 @@
+import PropTypes from 'prop-types';
 import UserNamePicture from '../home-page/UserNamePicture';
 import classes from './PostMenu.module.css';
 
 const PostMenu = props => {
   const { user } = props;
 
-  return user !== {} && user !== undefined ? (
-    <div className={classes.top}>
-      <div className={classes.nameSection}>
-        <UserNamePicture user={user} />
+  const hasUser = () => user !== {} && user !== undefined;
+
+  return (
+    hasUser() && (
+      <div className={classes.top}>
+        <DisplayUserDetails user={user} />
+        <MoreOptions />
       </div>
-      <div className={classes.right}>
-        <i className={`fas fa-ellipsis-h ${classes.more}`}></i>
-      </div>
-    </div>
-  ) : (
-    'Nothing to Display'
+    )
   );
 };
 
+const DisplayUserDetails = props => {
+  const { user } = props;
+  const hasUser = () => user !== {} && user !== undefined;
+  return (
+    hasUser() && (
+      <div className={classes.nameSection}>
+        <UserNamePicture user={user} />
+      </div>
+    )
+  );
+};
+
+const MoreOptions = props => {
+  return (
+    <div className={classes.right}>
+      <i className={`fas fa-ellipsis-h ${classes.more}`}></i>
+    </div>
+  );
+};
+
+PostMenu.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 export default PostMenu;
